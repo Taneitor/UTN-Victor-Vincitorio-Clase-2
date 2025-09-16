65eb1882169003a6c1c6c4d7fa80bb3171259ba7
Taneitor <vincitorio@gmail.com>
2025-09-15 21:03:29 -0300

    15-09-2025

--- Script.js
+++ Script.js
@@ -1,2 +1,28 @@
-const mensaje_contenedor_HTML = document.querySelector('#mensaje')
+const registros = [
+    {
+        nombre: "Victor Vincitorio",
+        edad: 57,
+        id: 1
+    }
+]
+
+const listado_registros_contenedor_HTML = document.querySelector('#listado')
+
+
+let registro_string_HTML = ''
+
+for(const registro of registros) {
+    const mensaje_string_HTML = `
+            <div>
+                <span>${registro.nombre}</span>
+                <span>${registro.edad}</span>
+                <hr/>
+            </div>
+        `
+    registro_string_HTML += mensaje_string_HTML 
+}
+
+listado_registros_contenedor_HTML.innerHTML = registro_string_HTML
+
+
 
